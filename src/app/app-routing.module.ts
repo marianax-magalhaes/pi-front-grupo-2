84b60425188dceaf2b82acc7afc05b0c6a05bde6
Murilo Leão <murilo.leao.pereira@gmail.com>
2021-10-25 19:41:13 -0300

    Tela de atualização de cadastro de usuário

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AtualizarCadastroComponent } from './components/atualizar-cadastro/atualizar-cadastro.component';
+import { HomeComponent } from './components/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:"", component: HomeComponent
+  },
+  {
+    path: "atualizar-cadastro", component: AtualizarCadastroComponent
+  }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
