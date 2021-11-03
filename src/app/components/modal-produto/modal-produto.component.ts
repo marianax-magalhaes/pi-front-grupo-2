bccabc346024f33ac2aaf514009dde9b307cd705
erictomson <erictomson@gmail.com>
2021-11-02 22:14:21 -0300

    Totalização do Pedido e botão Fechar Pedido

--- src/app/components/modal-produto/modal-produto.component.ts
+++ src/app/components/modal-produto/modal-produto.component.ts
@@ -76,6 +76,10 @@ export class ModalProdutoComponent implements OnInit {
     localStorage.setItem("carrinho", JSON.stringify(this.carrinho));
     // Resetando o formulário
     // this.formulario.reset();
+    // Fechando o modal
+    this.cancelar();
+    // Mostrando o carrinho atualizado
+    
   }
 
   RecuperarCarrinho(): void {
